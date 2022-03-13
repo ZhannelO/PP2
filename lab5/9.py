fdce538d78c48f5bef10f19d03eb40b3baf13802
zhannel <98833988+ZhannelO@users.noreply.github.com>
2022-03-13 21:16:56 +0600

    Add files via upload

---
+++ lab5/9.py
@@ -0,0 +1,5 @@
+#Write a Python program to insert spaces between words starting with capital letters.
+import re
+txt="SnakeCaseStringConverstToCamelCase"
+res=re.sub(r"(\w)([A-Z])",r"\1 \2",txt)
+print(res)
