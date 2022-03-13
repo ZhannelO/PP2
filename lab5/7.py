ff8b84299283887f454c41c4f27e87c7e4ac7835
zhannel <98833988+ZhannelO@users.noreply.github.com>
2022-03-13 20:52:38 +0600

    Add files via upload

---
+++ lab5/7.py
@@ -0,0 +1,8 @@
+#Write a python program to convert snake case string to camel case string.
+import re
+def camel(snake):
+    return snake.group(1)+snake.group(2).upper()
+REG = r"(.*?)_([a-zA-Z])"
+txt="Snake_case_string_converst_to_camel_case"
+res=re.sub(REG,camel,txt,0)
+print(res)
