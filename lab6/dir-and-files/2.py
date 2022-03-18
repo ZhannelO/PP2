d2dd30d1d53b7044bb3cbb0162107867d31b4f98
zhannel <98833988+ZhannelO@users.noreply.github.com>
2022-03-18 18:19:25 +0600

    Add files via upload

---
+++ lab6/dir-and-files/2.py
@@ -0,0 +1,7 @@
+#Write a Python program to check for access to a specified path.
+#  Test the existence, readability, writability and executability of the specified path
+import os
+print('Exist:', os.access('c:\\Users\\Public\\C programming library.docx', os.F_OK))
+print('Readable:', os.access('c:\\Users\\Public\\C programming library.docx', os.R_OK))
+print('Writable:', os.access('c:\\Users\\Public\\C programming library.docx', os.W_OK))
+print('Executable:', os.access('c:\\Users\\Public\\C programming library.docx', os.X_OK))
