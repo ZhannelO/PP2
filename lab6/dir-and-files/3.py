63a3af377e0ede5b97ac51dcc7f014b5fdeea6fc
zhannel <98833988+ZhannelO@users.noreply.github.com>
2022-03-18 18:21:41 +0600

    Add files via upload

---
+++ lab6/dir-and-files/3.py
@@ -0,0 +1,12 @@
+#Write a Python program to test whether a given path exists or not. 
+# If the path exist find the filename and directory portion of the given path.
+import os
+print("Test a path exists or not:")
+path = r'g:\\testpath\\a.txt'
+print(os.path.exists(path))
+path = r'g:\\testpath\\p.txt'
+print(os.path.exists(path))
+print("\nFile name of the path:")
+print(os.path.basename(path))
+print("\nDir name of the path:")
+print(os.path.dirname(path))
