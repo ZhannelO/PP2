9d01a7062b6afa2dba1e7906559c1102aea266a5
zhannel <98833988+ZhannelO@users.noreply.github.com>
2022-05-05 21:25:12 +0600

    Add files via upload

---
+++ lab10/1.py
@@ -0,0 +1,27 @@
+#Design tables for PhoneBook.
+import psycopg2
+
+config = psycopg2.connect(
+    host='localhost', 
+    database='postgres',
+    port=5433,
+    user='postgres',
+    password='12345'
+)
+
+current = config.cursor()
+
+current.execute(
+    '''
+    CREATE TABLE phonebook(
+        id INTEGER PRIMARY KEY,
+        username VARCHAR(20),
+        number VARCHAR(12),
+        email VARCHAR(30)
+    )
+    '''
+)
+
+config.commit()
+current.close()
+config.close()
